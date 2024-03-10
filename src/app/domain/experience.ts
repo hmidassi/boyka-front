40a259ea6db836152e2f6768803d6324b5c7edfc
hmidassi <hedy.midassi@gmail.com>
2024-03-10 11:04:56 +0100

    Updating experience with end_date, as optional

--- src/app/domain/experience.ts
+++ src/app/domain/experience.ts
@@ -3,6 +3,7 @@ import { MartialArt } from "./martial-arts";
 export interface Experience{
         club_name: string;
         starting_date: Date;
+        end_date?: Date;
         //one club may offer several martial arts at once
         martial_arts: MartialArt[];
         //find a smarter way to associate each martial art and its average duration per
